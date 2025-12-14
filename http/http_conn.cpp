f69a8d8aebb1676e2180c142f0ecff633ed2e156
yukixve <2382210031@qq.com>
2025-12-14 17:34:33 +0800

    Modify the HTTP header: content-type

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -530,6 +530,7 @@ bool http_conn::add_content_length(int content_len) {
 }
 bool http_conn::add_content_type() {
     return add_response("Content-Type:%s\r\n", "text/html");
+    // return add_response("Content-Type: %s\r\n", "text/html; charset=utf-8");
 }
 bool http_conn::add_linger() {
     return add_response("Connection:%s\r\n", (m_linger == true) ? "keep-alive" : "close");
